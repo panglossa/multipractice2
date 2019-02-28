c555e0418b7dc5de760ce2904392056a9381708f
panglossa <panglossa@yahoo.com.br>
2019-02-28 16:41:22 -0300

    updating location of the go!Johnny library

--- options.php
+++ options.php
@@ -5,8 +5,8 @@
 	*/
 $options = array(
 	'GJ_USETIDY' => false
-	, 'GJ_PATH_LOCAL' => '../gojohnny' /* Used by PHP. */
-	, 'GJ_PATH_WEB' => '../gojohnny' /* Used by JavaScript and CSS. */
+	, 'GJ_PATH_LOCAL' => './gojohnny' /* Used by PHP. */
+	, 'GJ_PATH_WEB' => './gojohnny' /* Used by JavaScript and CSS. */
 	, 'GJ_AUTOCLASS' => false /* Automatically assign a class attribute to every HTML tag, based on the go!Johnny class name of the object. */
 	, 'GJ_AUTOID' => false    /* Automatically assign a random ID attribute to every HTML tag generated. */
 	, 'GJ_AUTOMAIN' => true  /* Automatically generate a section or div with an id of 'main' which will be the default container for everything added to the page. */
