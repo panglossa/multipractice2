a68aa4ef48e198e259f513d401b365a5e8edbf2b
QVASIMODO <panglossa@yahoo.com.br>
2019-03-10 18:12:27 -0300

    2019-03-10 18:12:27

--- modules/home.php
+++ modules/home.php
@@ -8,7 +8,11 @@ $this->loadcourses();
 $i = 1;
 foreach($this->courses as $c) {
 	if ($i<=20) {
-		$lb_courses->add($c['name']);
+		$img = '';
+		if (trim($c['image'])!=''){
+			$img = "<img height=\"32\" src='data:image/png;base64," . base64_encode($c['image']) . "' />&nbsp;";
+			}
+		$lb_courses->add($img . $c['name']);
 		}
 	$i++;
 	}
@@ -20,7 +24,11 @@ $general->add(TH4('Currently Available Courses') . $lb_courses);
 if ($this->userid>-1) {
 	$lb_mycourses = TList();
 	foreach($this->mycourses as $c) {
-		$lb_mycourses->add($c['name']);
+		$img = '';
+		if (trim($c['image'])!=''){
+			$img = "<img height=\"32\" src='data:image/png;base64," . base64_encode($c['image']) . "' />&nbsp;";
+			}
+		$lb_mycourses->add($img . $c['name']);
 		}
 	if (count($lb_mycourses->items)>0) {
 		$mine->add(TH4('Your Current Courses') . $lb_mycourses);
